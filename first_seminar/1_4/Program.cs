8a8d45a9318ddc1f5b8fc83ec19ef5526aa63ec3
Nadezhda Shipilova <Nadin.v.sh@gmail.com>
2022-11-21 22:44:45 +0300

    task 4

---
+++ first_seminar/1_4/Program.cs
@@ -0,0 +1,6 @@
+﻿// 4. Напишите программу вычисления модуля числа.
+
+Console.Write("Введите число:");
+var s = Console.ReadLine();
+int N = s == null ? 0 : int.Parse(s);
+Console.WriteLine($"{Math.Abs(N)}");
