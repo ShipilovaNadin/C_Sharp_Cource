336d427304882da2dc9a478e3ddc4ae2b02ab2c9
Nadezhda Shipilova <Nadin.v.sh@gmail.com>
2022-11-22 22:49:28 +0300

    add task 4

---
+++ first_seminar/homework/0_4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число: ");
+string a = Console.ReadLine()!;
+int N = int.Parse(a);
+int n = 2;
+
+
+while(n < N)
+{
+    Console.WriteLine($"{n}");
+    n +=2;
+}
