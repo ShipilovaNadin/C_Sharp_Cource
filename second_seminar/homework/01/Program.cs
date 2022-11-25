63e854ac9093d4de22f70600321414bcc9f6f368
Nadezhda Shipilova <Nadin.v.sh@gmail.com>
2022-11-25 13:54:45 +0300

    add 1 HW task

---
+++ second_seminar/homework/01/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+string SecondNum(int num)
+{
+    if (num > 99 && num < 1000)
+        return $"{num / 10 % 10}";
+    else if (num > 1000)
+        return "wrong number";
+    else 
+    return "wrong number";
+}
+
+Console.WriteLine("enter number");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(SecondNum(number));
