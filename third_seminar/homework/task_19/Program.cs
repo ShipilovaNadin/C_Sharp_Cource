d85c9f7b81554afadb7348d76a7c0c228f5886d4
Nadezhda Shipilova <Nadin.v.sh@gmail.com>
2022-11-29 14:54:12 +0300

    add homework

---
+++ third_seminar/homework/task_19/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+int ReadNumber(string message)
+{
+    Console.Write(message);
+    var s = Console.ReadLine();
+    int a = s == null ? 0 : int.Parse(s);
+    return a;
+}
+
+void CheckPalindrom(int num)
+{
+    if (num < 100000 && num > 9999)
+    {
+        if ((num / 10000 == num % 10) && (num / 1000 % 10 == num / 10 % 10))
+        {
+            Console.WriteLine("-> да");
+        }
+        else
+        {
+            Console.WriteLine("-> нет");
+        }
+    }
+    else
+        Console.WriteLine("число не пятизначное");
+}
+
+int a = ReadNumber("Введите число:");
+CheckPalindrom(a);
---
+++ third_seminar/homework/task_21/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+void Cubes(int n)
+{
+    int i = 0;
+    double pere;
+    while (i < n)
+    {
+        i += 1;
+        if (i == (n))
+        {
+            pere = Math.Pow(i, 3);
+            Console.Write($"{pere}");
+        }
+        else
+        {
+            pere = Math.Pow(i, 3);
+            Console.Write($"{pere},");
+        }
+    }
+
+}
+Console.WriteLine("введите число");
+int N = int.Parse(Console.ReadLine());
+Cubes(N);
