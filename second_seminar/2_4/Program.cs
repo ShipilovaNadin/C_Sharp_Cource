7b4df359d2aec62c26cd1b7e07aaca846ecf2667
Nadezhda Shipilova <Nadin.v.sh@gmail.com>
2022-11-30 15:38:31 +0300

    add task in 2 seminar

---
+++ second_seminar/2_4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа, которая принимает на вход 2 числа и проверяет, является ли одно число квадратом другого.
+
+Console.Clear();
+
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+if (n * n == m || m * m == n)
+    Console.WriteLine("yes");
+else
+    Console.WriteLine("no");
