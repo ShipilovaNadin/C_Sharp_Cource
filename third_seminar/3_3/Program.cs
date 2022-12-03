39b4253e51326b0184cabe855cd8e66538ff7449
Nadezhda Shipilova <Nadin.v.sh@gmail.com>
2022-12-03 16:48:55 +0300

    and work on 4_lesson branch

--- third_seminar/3_3/Program.cs
+++ third_seminar/3_3/Program.cs
@@ -1,28 +1,30 @@
-﻿//  Напишите программу, которая принимает на вход число (N) и выдаёт таблицу
-// квадратов чисел от 1 до N.
+﻿//  Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
 
-void Chet(int N)
-{
-   int i = 0;
-   double pere;
-   while (i < N )
-   {
-        i += 1;
-        if (i == (N))
-        {
-            pere = Math.Pow(i,2);
-            Console.Write($"{pere}");
-        }
-        else
-        {
-        
-        pere = Math.Pow(i,2);
-        Console.Write($"{pere},");
-        }
-   }
-    
-   
-}
-Console.WriteLine("введите ч");
-int chislo = int.Parse(Console.ReadLine());
-Chet(chislo);
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+    Console.Write($"{i * i} ");
+
+// Вариант через цикл while
+// void Chet(int N)
+// {
+//    int i = 0;
+//    double pere;
+//    while (i < N )
+//    {
+//         i += 1;
+//         if (i == (N))
+//         {
+//             pere = Math.Pow(i,2);
+//             Console.Write($"{pere}");
+//         }
+//         else
+//         {
+//         pere = Math.Pow(i,2);
+//         Console.Write($"{pere},");
+//         }
+//    }
+// }
+// Console.WriteLine("введите ч");
+// int chislo = int.Parse(Console.ReadLine());
+// Chet(chislo);
