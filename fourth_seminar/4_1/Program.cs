734be659b56317be67cf881b0e8a6c1b696d2214
Nadezhda Shipilova <Nadin.v.sh@gmail.com>
2022-12-05 19:42:44 +0300

    and work on 4 seminar

--- fourth_seminar/4_1/Program.cs
+++ fourth_seminar/4_1/Program.cs
@@ -1,21 +1,26 @@
 ﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
-
-int Amount(long num)
-{
-    if(num == 0)
-    {
-        return(1);
-    }
-    int count = 0;
-    while (num > 0)
-    {
-        num = num / 10;
-        count++;
-    }
-    return count;
-}
-
+// int Amount(long num)
+// {
+//     if(num == 0)
+//     {
+//         return(1); 
+//     }
+//     int count = 0;
+//     while (num > 0)
+//     {
+//         num = num / 10;
+//         count++;
+//     }
+//     return count;
+// }
+// Console.Write("Введите число: ");
+// Console.WriteLine(Amount(long.Parse(Console.ReadLine())));
+//  вариант 2
 Console.Write("Введите число: ");
-
-Console.WriteLine(Amount(long.Parse(Console.ReadLine())));
-
+int n = Convert.ToInt32(Console.ReadLine()), count = 1;
+while (n / 10 != 0)
+{
+    n /= 10;
+    count++;
+}
+Console.WriteLine(count);
