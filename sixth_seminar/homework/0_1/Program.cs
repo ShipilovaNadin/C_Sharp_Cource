80a7f98b07a7030e6414abc27a965dddf5546cb0
Nadezhda Shipilova <Nadin.v.sh@gmail.com>
2022-12-11 17:00:44 +0300

    add homework 6

---
+++ sixth_seminar/homework/0_1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+        Console.Write($"{arr[i]} ");
+    Console.WriteLine();
+}
+
+Console.Write("Введите элементы(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+Print(arr);
+Console.WriteLine($"Кол-во элементов больше нуля: {count}");
+
+
---
+++ sixth_seminar/homework/0_2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+void FindXY()
+{
+    Console.Write("Задайте значение b1: ");
+    double b1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Задайте значение k1: ");
+    double k1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Задайте значение b2: ");
+    double b2 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Задайте значение k2: ");
+    double k2 = Convert.ToDouble(Console.ReadLine());
+
+    //k2 * x + b2 = k1 * x + b1 -> x = (b2 - b1) / (k1 - k2)
+    if (k1 == k2) // угловые коэффициенты равны
+        Console.WriteLine("Прямые параллельны.");
+    else if (k1 / k2 == b1 / b2)
+        Console.WriteLine("Прямые совпадают.");
+    else
+    {
+        double x = (b2 - b1) / (k1 - k2);
+        double y = k1 * x + b1;
+        Console.WriteLine($"Координаты точки пересечения прямых равны: ({x};{y}).");
+    }
+}
+
+FindXY();
