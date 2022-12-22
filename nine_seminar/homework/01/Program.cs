60c27ca54cba89f09d4c6dc0e6453ccc372a528b
Nadezhda Shipilova <Nadin.v.sh@gmail.com>
2022-12-22 11:44:44 +0300

    homework 9

--- nine_seminar/homework/01/Program.cs
+++ nine_seminar/homework/01/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значения M и N. Напишите программу, которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+
+void Numbers(int m, int n)
+{
+    if (m > n) return;
+    if (m % 2 == 0)
+    {
+        Console.Write($" {m} ");
+        Numbers(m + 2, n);
+    }
+    else
+    {
+        m++;
+        Console.Write($" {m} ");
+        Numbers(m + 2, n);
+    }
+}
+Console.WriteLine("Введите значение числа m: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение числа n: ");
+int n = int.Parse(Console.ReadLine());
+Numbers(m, n);
+
---
+++ nine_seminar/homework/02/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N с помощью рекурсии.
+
+int Summ(int m, int n)
+{
+    if (m > n) return 0;
+    return m + Summ(m + 1, n);
+}
+
+Console.WriteLine("Введите значение числа m: ");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите значение числа n: ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Cумма натуральных элементов в промежутке от M до N составляет: {Summ(m, n)}");
